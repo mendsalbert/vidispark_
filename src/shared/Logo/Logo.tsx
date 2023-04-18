12d90b25c360e2437f5536a32d2e33082d3b5874
mendsalbert <mendsalbert@gmail.com>
2023-04-18 15:30:23 +0000

    vidispark

--- src/shared/Logo/Logo.tsx
+++ src/shared/Logo/Logo.tsx
@@ -35,7 +35,7 @@ const Logo: React.FC<LogoProps> = ({
           alt="Logo-Light"
         />
       )} */}
-      <span className=" bg-[#008faf] rounded-full">
+      <span className="p-4 bg-[#008faf] rounded-full">
         <span className="font-semibold text-3xl">🎥</span>
       </span>
     </Link>
