bf630e31dc6bd02b1d85636b4f1a4b1f8d0e715b
mendsalbert <mendsalbert@gmail.com>
2023-04-18 22:49:31 +0000

    vidispark

--- src/components/Leaderboard/SectionGridLeaderboard.tsx
+++ src/components/Leaderboard/SectionGridLeaderboard.tsx
@@ -55,20 +55,7 @@ const SectionGridLeaderboard: FC<SectionGridAuthorBoxProps> = ({
   const renderHeading1 = () => {
     return (
       <div className="mb-12 lg:mb-16  flex justify-between flex-col sm:flex-row">
-        <Heading
-          rightPopoverText="Creators"
-          rightPopoverOptions={[
-            {
-              name: "Creators",
-              href: "#",
-            },
-            {
-              name: "Buyers",
-              href: "#",
-            },
-          ]}
-          className="text-neutral-900 dark:text-neutral-50"
-        >
+        <Heading className="text-neutral-900 dark:text-neutral-50">
           Leaderboard
         </Heading>
       </div>
