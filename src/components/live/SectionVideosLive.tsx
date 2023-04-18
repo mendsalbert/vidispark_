0bdae242e3864129eb80ef4e22a4e3ff123aad81
mendsalbert <mendsalbert@gmail.com>
2023-04-18 16:20:49 +0000

    vidispark

--- src/components/live/SectionVideosLive.tsx
+++ src/components/live/SectionVideosLive.tsx
@@ -74,9 +74,9 @@ const SectionVideosLive: FC<SectionVideosLiveProps> = ({
           perspectives on just about any topic. Everyone’s welcome."
       >
         🎬 You are live{" "}
-        <span className="relative flex h-3 w-3 ml-2">
-          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-          <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+        <span className="relative flex h-6 w-6 ml-2">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-red-600"></span>
         </span>
       </Heading>
 
