9c4c54fcfd2e41fea3e996235fa7edf4a9b09b18
mendsalbert <mendsalbert@gmail.com>
2023-04-18 16:09:56 +0000

    vidispark

--- src/components/SectionHero/SectionHero2.tsx
+++ src/components/SectionHero/SectionHero2.tsx
@@ -43,18 +43,19 @@ const SectionHero2: FC<SectionHero2Props> = ({ className = "", children }) => {
                 </svg>
               </span>
             </ButtonPrimary>
-            <Link to="/live">
-              <ButtonPrimary
-                sizeClass="px-4 py-2 sm:px-5"
-                className="bg-red-500  hover:bg-red-700"
-              >
+
+            <ButtonPrimary
+              sizeClass="px-4 py-2 sm:px-5"
+              className="bg-red-500  hover:bg-red-700"
+            >
+              <Link to="/live">
                 Go Live
                 <span className="relative flex h-3 w-3 ml-2">
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-100 opacity-75"></span>
                   <span className="relative inline-flex rounded-full h-3 w-3 bg-gray-200"></span>
                 </span>
-              </ButtonPrimary>
-            </Link>
+              </Link>
+            </ButtonPrimary>
           </div>
         </div>
         <div className="flex-grow">
