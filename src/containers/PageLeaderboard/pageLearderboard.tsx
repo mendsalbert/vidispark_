acfd1be7aae98b39a2e25bc2520045713e5e4c64
mendsalbert <mendsalbert@gmail.com>
2023-04-18 22:54:26 +0000

    vidispark

--- src/containers/PageLeaderboard/pageLearderboard.tsx
+++ src/containers/PageLeaderboard/pageLearderboard.tsx
@@ -1,4 +1,6 @@
+import BackgroundSection from "components/BackgroundSection/BackgroundSection";
 import SectionGridLeaderboard from "components/Leaderboard/SectionGridLeaderboard";
+import SectionGridAuthorBox from "components/SectionGridAuthorBox/SectionGridAuthorBox";
 import React from "react";
 
 type Props = {};
@@ -7,7 +9,12 @@ const PageLearderboard = (props: Props) => {
   return (
     <div className="nc-PageHome relative overflow-hidden">
       <div className="container relative space-y-24 my-24 lg:space-y-32 lg:my-32">
-        <SectionGridLeaderboard />
+        <BackgroundSection />
+        <SectionGridAuthorBox
+          sectionStyle="style2"
+          data={Array.from("11111111")}
+          boxCard="box4"
+        />{" "}
       </div>
     </div>
   );
