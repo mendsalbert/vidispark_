81228d415399710d75d8d96a07fa67e29034a23a
mendsalbert <mendsalbert@gmail.com>
2023-04-18 16:01:51 +0000

    vidispark

--- src/components/Header/MainNav2Logged.tsx
+++ src/components/Header/MainNav2Logged.tsx
@@ -61,7 +61,10 @@ const MainNav2Logged: FC<MainNav2LoggedProps> = () => {
             </div>
             <div></div>
             <ButtonPrimary sizeClass="px-4 py-2 sm:px-5">Create</ButtonPrimary>
-            <ButtonPrimary sizeClass="px-4 py-2 sm:px-5" className="bg-red-500">
+            <ButtonPrimary
+              sizeClass="px-4 py-2 sm:px-5"
+              className="bg-red-500 space-x-2"
+            >
               Live
               <span className="relative flex h-3 w-3">
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-100 opacity-75"></span>
