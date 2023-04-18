d23ed674a9fbf42f1f4b75a3809b499b848be3ea
mendsalbert <mendsalbert@gmail.com>
2023-04-18 20:07:43 +0000

    vidispark

--- src/containers/PageLeaderboard/pageLearderboard.tsx
+++ src/containers/PageLeaderboard/pageLearderboard.tsx
@@ -4,7 +4,13 @@ import React from "react";
 type Props = {};
 
 const PageLearderboard = (props: Props) => {
-  return <SectionGridLeaderboard />;
+  return (
+    <div className="nc-PageHome relative overflow-hidden">
+      <div className="container relative space-y-24 my-24 lg:space-y-32 lg:my-32">
+        <SectionGridLeaderboard />
+      </div>
+    </div>
+  );
 };
 
 export default PageLearderboard;
