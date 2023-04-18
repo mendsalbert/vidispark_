0a41336a3da19068bfd9c0f9ed90187719c865b3
mendsalbert <mendsalbert@gmail.com>
2023-04-18 16:17:22 +0000

    vidispark

--- src/containers/pageLive/PageLive.tsx
+++ src/containers/pageLive/PageLive.tsx
@@ -1,9 +1,16 @@
+import SectionVideos from "containers/PageHome/SectionVideos";
 import React from "react";
 
 type Props = {};
 
 const PageLive = (props: Props) => {
-  return <div>PageLive</div>;
+  return (
+    <div className="nc-PageHome relative overflow-hidden">
+      <div className="container relative space-y-24 my-24 lg:space-y-32 lg:my-32">
+        <SectionVideos />
+      </div>
+    </div>
+  );
 };
 
 export default PageLive;
