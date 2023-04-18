c3bf4a978291d90b499a633dcb95374c66508edd
mendsalbert <mendsalbert@gmail.com>
2023-04-18 16:00:47 +0000

    vidispark

--- src/components/Header/MainNav2Logged.tsx
+++ src/components/Header/MainNav2Logged.tsx
@@ -63,7 +63,10 @@ const MainNav2Logged: FC<MainNav2LoggedProps> = () => {
             <ButtonPrimary sizeClass="px-4 py-2 sm:px-5">Create</ButtonPrimary>
             <ButtonPrimary sizeClass="px-4 py-2 sm:px-5" className="bg-red-500">
               Live
-              <span className="animate-ping  inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+              <span className="relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+              </span>
             </ButtonPrimary>
             <div></div>
             <AvatarDropdown />
