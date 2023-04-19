98dc1003f8e6c4427cc89b3fd6c1071b5ca93bdd
mendsalbert <mendsalbert@gmail.com>
2023-04-19 20:47:19 +0000

    vidispark

--- src/components/Header/MainNav2Logged.tsx
+++ src/components/Header/MainNav2Logged.tsx
@@ -60,7 +60,7 @@ const MainNav2Logged: FC<MainNav2LoggedProps> = () => {
               <NotifyDropdown />
             </div>
             <div></div>
-            <ButtonPrimary sizeClass="px-4 py-2 sm:px-5">Create</ButtonPrimary>
+            {/* <ButtonPrimary sizeClass="px-4 py-2 sm:px-5">Create</ButtonPrimary> */}
 
             <div></div>
             <AvatarDropdown />
