baf578a8ee18941549fcd7d864158fbf9218ada5
mendsalbert <mendsalbert@gmail.com>
2023-04-19 18:28:11 +0000

    vidispark

--- src/containers/PageMarketplace/PageMarketplace.tsx
+++ src/containers/PageMarketplace/PageMarketplace.tsx
@@ -5,8 +5,10 @@ type Props = {};
 
 const PageMarketplace = (props: Props) => {
   return (
-    <div>
-      <Marketplace boxCard="box3" />
+    <div className="nc-PageHome relative overflow-hidden">
+      <div className="container relative space-y-24 my-24 lg:space-y-32 lg:my-32">
+        <Marketplace boxCard="box3" />
+      </div>
     </div>
   );
 };
