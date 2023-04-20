35b16404d58475e5d35a9d0503a724e3d04b32a3
mendsalbert <mendsalbert@gmail.com>
2023-04-20 15:58:07 +0000

    vidispark

--- src/containers/PageCollection.tsx
+++ src/containers/PageCollection.tsx
@@ -39,7 +39,7 @@ const PageCollection: FC<PageCollectionProps> = ({ className = "" }) => {
           <div className=" bg-white dark:bg-neutral-900  p-4 dark:border dark:border-neutral-700 rounded-3xl md:rounded-[40px] shadow-xl flex flex-col md:flex-row lg:items-center">
             <Nav
               className="p-1 bg-white flex flex-col dark:bg-neutral-800 rounded-full shadow-lg"
-              containerClassName=" relative flex flex-col justify-center w-full text-sm md:text-base"
+              containerClassName=" relative flex flex-col h-full justify-center w-full text-sm md:text-base"
             >
               {[
                 {
