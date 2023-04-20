cc0292690eb0a2166adc8e18f53f1d47a0ccc51e
mendsalbert <mendsalbert@gmail.com>
2023-04-20 15:52:33 +0000

    vidispark

--- src/containers/PageCollection.tsx
+++ src/containers/PageCollection.tsx
@@ -52,7 +52,7 @@ const PageCollection: FC<PageCollectionProps> = ({ className = "" }) => {
               `,
                 },
                 {
-                  name: "Lives",
+                  name: "Live",
                   icon: `<svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M18.5 19.5H14.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M16.5 21.5V17.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
