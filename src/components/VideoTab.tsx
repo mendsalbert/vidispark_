00ca1c8b5dc2fcbb2d18aa41466c33cc16e4d4e1
mendsalbert <mendsalbert@gmail.com>
2023-04-21 11:07:53 +0000

    vidispark

--- src/components/VideoTab.tsx
+++ src/components/VideoTab.tsx
@@ -56,56 +56,56 @@ const VideoTab: FC<SectionSliderCardNftVideoProps> = ({
         >
           Recently uploaded Videos
         </Heading>
-        <div className="glide__track" data-glide-el="track">
-          <ul className="glide__slides">
-            {[
-              "https://images.unsplash.com/photo-1643101809204-6fb869816dbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-              "https://images.unsplash.com/photo-1643101808200-0d159c1331f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-              "https://images.unsplash.com/photo-1643101808513-0552e31e4d9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-              "https://images.unsplash.com/photo-1638913974023-cef988e81629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-            ].map((item, index) => (
-              <li key={index} className={`glide__slide ${itemClassName}`}>
-                <CardNFTVideo featuredImage={item} />
-              </li>
-            ))}
-            <li className={`glide__slide  ${itemClassName}`}>
-              <Link to={"/page-search"} className="block relative group">
-                <div className="flex aspect-w-16 aspect-h-9 w-full h-0 rounded-3xl bg-neutral-100 dark:bg-neutral-800"></div>
-                <div className="absolute inset-y-6 inset-x-10  flex flex-col items-center justify-center">
-                  <div className="flex items-center justify-center relative">
-                    <span className="text-xl font-semibold">NFTs Video</span>
-                    <svg
-                      className="absolute left-full w-5 h-5 ml-2 rotate-45 group-hover:scale-110 transition-transform"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M18.0701 9.57L12.0001 3.5L5.93005 9.57"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeMiterlimit="10"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M12 20.4999V3.66992"
-                        stroke="currentColor"
-                        strokeWidth="1.5"
-                        strokeMiterlimit="10"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </div>
-                  <span className="text-sm mt-1">Show me more</span>
-                </div>
-              </Link>
+        {/* <div className="glide__track" data-glide-el="track"> */}
+        <ul className="glide__slides">
+          {[
+            "https://images.unsplash.com/photo-1643101809204-6fb869816dbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+            "https://images.unsplash.com/photo-1643101808200-0d159c1331f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+            "https://images.unsplash.com/photo-1643101808513-0552e31e4d9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+            "https://images.unsplash.com/photo-1638913974023-cef988e81629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+          ].map((item, index) => (
+            <li key={index} className={`glide__slide ${itemClassName}`}>
+              <CardNFTVideo featuredImage={item} />
             </li>
-          </ul>
-        </div>
+          ))}
+          <li className={`glide__slide  ${itemClassName}`}>
+            <Link to={"/page-search"} className="block relative group">
+              <div className="flex aspect-w-16 aspect-h-9 w-full h-0 rounded-3xl bg-neutral-100 dark:bg-neutral-800"></div>
+              <div className="absolute inset-y-6 inset-x-10  flex flex-col items-center justify-center">
+                <div className="flex items-center justify-center relative">
+                  <span className="text-xl font-semibold">NFTs Video</span>
+                  <svg
+                    className="absolute left-full w-5 h-5 ml-2 rotate-45 group-hover:scale-110 transition-transform"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M18.0701 9.57L12.0001 3.5L5.93005 9.57"
+                      stroke="currentColor"
+                      strokeWidth="1.5"
+                      strokeMiterlimit="10"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                    <path
+                      d="M12 20.4999V3.66992"
+                      stroke="currentColor"
+                      strokeWidth="1.5"
+                      strokeMiterlimit="10"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+                <span className="text-sm mt-1">Show me more</span>
+              </div>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
+    // </div>
   );
 };
 
