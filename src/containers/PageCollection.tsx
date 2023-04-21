8d19f1040283347d0add794d11d86ddbc7e8416c
mendsalbert <mendsalbert@gmail.com>
2023-04-21 09:55:58 +0000

    vidispark

--- src/containers/PageCollection.tsx
+++ src/containers/PageCollection.tsx
@@ -127,17 +127,7 @@ const PageCollection: FC<PageCollectionProps> = ({ className = "" }) => {
                  </svg>
               `,
                   },
-                  {
-                    name: " Start Live",
-                    icon: `<svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M18.364 19.364a9 9 0 1 0 -12.728 0"></path>
-                    <path d="M15.536 16.536a5 5 0 1 0 -7.072 0"></path>
-                    <path d="M12 13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
-                 </svg>
-              
-              `,
-                  },
+
                   {
                     name: "Leaderboard",
                     icon: `<svg xmlns="http://www.w3.org/2000/svg"  class="w-7 h-7" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -149,6 +139,17 @@ const PageCollection: FC<PageCollectionProps> = ({ className = "" }) => {
                     <path d="M14 10h2"></path>
                  </svg>
                `,
+                  },
+                  {
+                    name: " Start Live",
+                    icon: `<svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M18.364 19.364a9 9 0 1 0 -12.728 0"></path>
+                    <path d="M15.536 16.536a5 5 0 1 0 -7.072 0"></path>
+                    <path d="M12 13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                 </svg>
+              
+              `,
                   },
                   {
                     name: "Followers",
