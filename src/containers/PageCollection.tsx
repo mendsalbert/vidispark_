dd3edda58c55f0cd308b690109e4f80c2ea30002
mendsalbert <mendsalbert@gmail.com>
2023-04-21 10:08:15 +0000

    vidispark

--- src/containers/PageCollection.tsx
+++ src/containers/PageCollection.tsx
@@ -140,17 +140,6 @@ const PageCollection: FC<PageCollectionProps> = ({ className = "" }) => {
                  </svg>
                `,
                   },
-                  {
-                    name: " Start Live",
-                    icon: `<svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8rounded-full text-red-500" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M18.364 19.364a9 9 0 1 0 -12.728 0"></path>
-                    <path d="M15.536 16.536a5 5 0 1 0 -7.072 0"></path>
-                    <path d="M12 13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
-                 </svg>
-              
-              `,
-                  },
 
                   {
                     name: "Marketplace",
@@ -164,6 +153,18 @@ const PageCollection: FC<PageCollectionProps> = ({ className = "" }) => {
                  </svg>
                `,
                   },
+                  {
+                    name: " Start Live",
+                    icon: `<svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8rounded-full text-red-500" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M18.364 19.364a9 9 0 1 0 -12.728 0"></path>
+                    <path d="M15.536 16.536a5 5 0 1 0 -7.072 0"></path>
+                    <path d="M12 13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                 </svg>
+              
+              `,
+                  },
+
                   {
                     name: "Vidisverse",
                     icon: `<svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
